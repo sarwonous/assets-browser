8e08db6faadd8bbde5961ba261f0f09072749cdf
suga <agus.sarwono@gmail.com>
2022-02-13 18:48:57 +0700

    use env when not using json file

--- src/libs/cloud.js
+++ src/libs/cloud.js
@@ -1,10 +1,31 @@
 import { Storage } from "@google-cloud/storage";
 import path from "path";
-import os from "os";
+import fs from "fs";
 
-const storage = new Storage({
-    keyFilename: process.env.CREDENTIAL_FILE
-});
+let options = {};
+
+if (fs.existsSync(process.env.CREDENTIAL_FILE)) {
+    options = {
+        keyFilename: process.env.CREDENTIAL_FILE,
+    };
+} else {
+    options = {
+        credentials: {
+            "type": process.env.GOOGLE_TYPE,
+            "project_id": process.env.GOOGLE_PROJECT_ID,
+            "private_key_id": process.env.GOOGLE_PRIVATE_KEY_ID,
+            "private_key": process.env.GOOGLE_PRIVATE_KEY,
+            "client_email": process.env.GOOGLE_CLIENT_EMAIL,
+            "client_id": process.env.GOOGLE_CLIENT_ID,
+            "auth_uri": process.env.GOOGLE_AUTH_URI,
+            "token_uri": process.env.GOOGLE_TOKEN_URI,
+            "auth_provider_x509_cert_url": process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+            "client_x509_cert_url": process.env.GOOGLE_CLIENT_X509_CERT_URL
+        }
+    };
+}
+
+const storage = new Storage(options);
 
 export const getFiles = async(options = {}) => {
     try {
