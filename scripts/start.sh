c03de2829c2d49b3ea5f3bc3ba105ef7a87c9dce
suga <agus.sarwono@gmail.com>
2022-02-14 22:17:41 +0700

    update README.md

---
+++ scripts/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd ./
+if [ -z "$BUCKET_NAME" ]
+then
+    if [ -z "$CREDENTIAL_FILE" ]
+    then
+        echo "No bucket and creds name provided"
+        exit 1
+    else
+        echo "No bucket name provided"
+        exit 1
+    fi
+else 
+    echo "Bucket name: $BUCKET_NAME"
+    echo "Credential file: $CREDENTIAL_FILE"
+    echo "Starting..."
+    yarn build
+    yarn start
+fi
